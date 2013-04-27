cd285cca3defceafe9ba49042fd08bdc377148ad
sheddj2 <sheddj2@ubuntu.(none)>
2013-04-27 15:04:00 -0400

    Updated the notary file, just needs authentication and packet transfer stuff

--- notary.py
+++ notary.py
@@ -2,23 +2,38 @@
 
 import Crypto
 import Crypto.PublicKey.RSA
+from Crypto.Random.random import getrandbits
+
+#Generate random bits, send to voter
+
+#Get randombits back signed with vote private key
+
+# iterate through all public keys to see if one is a valid signature
+
+
 
 #Genrate a Notary key pair
 NotaryKey = RSA.generate(1024)
+NotaryPublic = NotaryKey.publickey()
 
 #publish Notary public key to a file
-
+f = open('NotaryKey.pem','w')
+f.write(NotaryPublic.exportKey())
+f.close()
 
 #Recieve blinded, encrypted vote from voter
 #blindedC
 
 
-#authenticate with token
 
 
-k = 123L #Change this to something random
+
+k = getrandbits(64) #Change this to something random
 #blind sign c'
-NotaryKey.sign( blindedC, k)
+blindsigned = NotaryKey.sign( blindedC, k)
+
+
+#sent blindsigned back to the voter
 
 
 
