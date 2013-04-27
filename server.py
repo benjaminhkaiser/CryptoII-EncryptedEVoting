ad54c68f87fb97e62d999666764942fb3c9e48c2
jeremy <jeremy@jeremy-VirtualBox.(none)>
2013-04-27 15:26:45 -0400

    RSA asymmetric encryption for voter->server communication implemented

--- server.py
+++ server.py
@@ -1,27 +1,39 @@
 import socket
-from Crypto.Cipher import AES	#requires pyCrypto installation
+from Crypto.PublicKey import RSA
+from Crypto import Random
 
+publicKeyFile = "serverpubkey.pem"
+
+#writes public key to file, returns private key
+def set_keys():
+	rand = Random.new().read
+	keys = RSA.generate(1024,rand)
+	public_key = keys.publickey().exportKey()
+	private_key = keys.exportKey(passphrase="cryptoII")
+
+	pubHandle = open(publicKeyFile, 'w')
+	pubHandle.write(public_key)
+	
+	return keys 
 
 def listen_for_client():
-	s = socket.socket()			#create the socket
+	key = set_keys()	#generate keys
+	s = socket.socket()	#create the socket
 	host = socket.gethostname()	#get the host name of the socket
-	port = 12345				#initialize the port to connect over
-	s.bind((host, port))		#bind the socket to the port
+	port = 12345	#initialize the port to connect over
+	s.bind((host, port))	#bind the socket to the port
 
-	s.listen(5)					#listen for client connections
+	s.listen(5)	#listen for client connections
 	while True:
-			c, addr = s.accept()				#accept client connection
-			votestring = str(addr) + " voted "	#add client address to string
-			vote = c.recv(1024)
-			iv = c.recv(16)
+		c, addr = s.accept()	#accept client connection
+		votestring = str(addr) + " voted "	#add client address to string
+		vote = c.recv(128)
 
-			decryptor = AES.new('0123456789abcdef', AES.MODE_CBC, iv)
-			vote = decryptor.decrypt(vote)	
-
-			votestring += str(vote)[0]
-			print votestring
-			c.send("Vote registered.")			#send confirmation msg
-			c.close()							#close client connection
+		votestring += key.decrypt(vote)
+		
+		print votestring
+		c.send("Vote registered.")	#send confirmation msg
+		c.close()	#close client connection
 
 if __name__ == "__main__":
-    listen_for_client()			
+	listen_for_client()			
--- voter.py
+++ voter.py
@@ -2,33 +2,31 @@ import socket
 import hashlib
 
 from Crypto.Cipher import AES	#requires pyCrypto installation
+from Crypto.PublicKey import RSA	#requires pyCrypto installation
 from Crypto import Random		#requires pyCrypto installation
 
-iv = Random.new().read(16)
-
-def get_vote(key):
+def get_vote():
 	vote = raw_input("Enter your vote: ")	#get vote from user
-	while (len(vote) < 1024):
-			vote += " "
-	encryptor = AES.new(key,AES.MODE_CBC,iv)
-	vote = encryptor.encrypt(vote)
-	return vote
+	while (len(vote) < 64):
+		vote += " "
+	f = open("serverpubkey.pem",'r')
+	key = RSA.importKey(f.read()) 
+	enc_data = key.encrypt(vote,32)
+	print(len(enc_data[0]))	
+	return enc_data[0]
 
 def connect_to_server():
 	sock = socket.socket()		#create a socket
 	host = socket.gethostname()	#get the host name of the socket
 	port = 12345              	#initialize the port for the socket
 
-	#key = hashlib.sha256(host).digest()	#generate a key using hostname as seed
-	key = '0123456789abcdef'
-
-	vote = get_vote(key)
+	vote = get_vote()
 
 	sock.connect((host, port))	#connect the socket
-	sock.send(vote)				#send the user's vote over the socket
-	sock.send(iv)
+	sock.send(vote)			#send the user's vote over the socket
+
 	print sock.recv(1024)		#print rec'd confirmation msg
-	sock.close					#close the socket
+	sock.close			#close the socket
 
 if __name__ == "__main__":
 	connect_to_server()	
