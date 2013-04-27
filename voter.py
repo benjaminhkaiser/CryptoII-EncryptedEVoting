66463c199cc80578f3277cbf59e05c39ee334566
benjaminhkaiser <benjamin.h.kaiser@gmail.com>
2013-04-27 17:50:15 -0400

    RSA used to send AES session key to be used for future communication

--- voter.py
+++ voter.py
@@ -1,29 +1,41 @@
 import socket
 import hashlib
 
-from Crypto.Cipher import AES	#requires pyCrypto installation
+from Crypto.Cipher import AES		#requires pyCrypto installation
 from Crypto.PublicKey import RSA	#requires pyCrypto installation
 from Crypto import Random		#requires pyCrypto installation
 
-def get_vote():
+def get_vote(key,iv):
 	vote = raw_input("Enter your vote: ")	#get vote from user
+
+	#pad vote
 	while (len(vote) < 64):
 		vote += " "
-	f = open("serverpubkey.pem",'r')
-	key = RSA.importKey(f.read()) 
-	enc_data = key.encrypt(vote,32)
-	print(len(enc_data[0]))	
-	return enc_data[0]
 
+	#encrypt with AES
+	encryptor = AES.new(key,AES.MODE_CBC,iv)
+	return encryptor.encrypt(vote)
+	
 def connect_to_server():
 	sock = socket.socket()		#create a socket
 	host = socket.gethostname()	#get the host name of the socket
 	port = 12345              	#initialize the port for the socket
 
-	vote = get_vote()
-
 	sock.connect((host, port))	#connect the socket
-	sock.send(vote)			#send the user's vote over the socket
+
+	#generate AES key and iv
+	AES_key = Random.new().read(16)
+	AES_iv = Random.new().read(16)
+
+	#encrypt AES data with server's RSA public key
+	f = open("serverpubkey.pem",'r')
+	RSA_key = RSA.importKey(f.read())
+	enc_AES_key = RSA_key.encrypt(AES_key,32)
+	enc_AES_iv = RSA_key.encrypt(AES_iv,32)
+
+	sock.send(enc_AES_key[0])	#send the AES key
+	sock.send(enc_AES_iv[0])	#send the AES iv
+	sock.send(get_vote(AES_key,AES_iv)) #send AES-encrypted vote
 
 	print sock.recv(1024)		#print rec'd confirmation msg
 	sock.close			#close the socket
