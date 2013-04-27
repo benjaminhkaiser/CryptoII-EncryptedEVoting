0f201a67b08ebd62b508b4778e46804a27c6b1fa
sheddj2 <sheddj2@ubuntu.(none)>
2013-04-27 17:55:21 -0400

    Just making sure this is the most current version of notary

--- notary.py
+++ notary.py
@@ -3,38 +3,51 @@
 import Crypto
 import Crypto.PublicKey.RSA
 from Crypto.Random.random import getrandbits
+import os
 
 #Generate random bits, send to voter
-
+randomBits = getrandbits(64)
 #Get randombits back signed with vote private key
+signedRandomBits
 
+#Set this to true only if you get a valid signature verification
+isValidUser = false
 # iterate through all public keys to see if one is a valid signature
+#length of valid public key after export is always 217
+statinfo = os.stat('RegKeys.pem')
+filesize = stat_info.st_size
+f = open('RegKeys.pem', 'r')
+if (filesize%217 == 0):
+	for x in range(0, filesize/217):
+		tPubKey = RSA.importKey(f.read(271))
+		if tPubKey.verify(randomBits, signedRandomBits):
+			isValidUser = true
+			x = filesize/217#This just terminates the for loop
+		
+if isValidUser:
+
+	#Genrate a Notary key pair
+	NotaryKey = RSA.generate(1024)
+	NotaryPublic = NotaryKey.publickey()
+
+	#publish Notary public key to a file
+	f = open('NotaryKey.pem','w')
+	f.write(NotaryPublic.exportKey())
+	f.close()
+
+	#Recieve blinded, encrypted vote from voter
+	blindedC
 
 
 
-#Genrate a Notary key pair
-NotaryKey = RSA.generate(1024)
-NotaryPublic = NotaryKey.publickey()
-
-#publish Notary public key to a file
-f = open('NotaryKey.pem','w')
-f.write(NotaryPublic.exportKey())
-f.close()
-
-#Recieve blinded, encrypted vote from voter
-#blindedC
+	k = getrandbits(64) #Change this to something random
+	#blind sign c'
+	blindsigned = NotaryKey.sign( blindedC, k)
 
 
+	#send blindsigned back to the voter
 
 
-
-k = getrandbits(64) #Change this to something random
-#blind sign c'
-blindsigned = NotaryKey.sign( blindedC, k)
-
-
-#sent blindsigned back to the voter
-
-
+#Optional behaviour when user is not valid, scolding etc...
 
 
